3355efb22eca7d63035a7720f66a1a0dff9fe9a4
jjehu <144302215+jjehu@users.noreply.github.com>
2024-09-01 13:24:07 -0400

    Delete t1/pyto directory

--- t1/pyto/src/main/java/com/pyto/core/repository/DepartamentoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.pyto.core.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.pyto.core.model.DepartamentoModel;
-
-public interface DepartamentoRepository extends JpaRepository<DepartamentoModel, Integer>{
-
-}
--- t1/pyto/src/main/java/com/pyto/core/repository/PersonaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.pyto.core.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.pyto.core.model.PersonaModel;
-
-public interface PersonaRepository extends JpaRepository<PersonaModel, Integer>{
-
-}
