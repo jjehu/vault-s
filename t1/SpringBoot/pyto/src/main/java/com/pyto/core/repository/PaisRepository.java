900d78ee1893bbd8dc7b667e409122d3e2201964
jjehu <144302215+jjehu@users.noreply.github.com>
2024-09-05 22:23:59 -0400

    Delete t1/SpringBoot/pyto directory

--- t1/SpringBoot/pyto/src/main/java/com/pyto/core/repository/PaisRepository.java
+++
@@ -1,9 +0,0 @@
-package com.pyto.core.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.pyto.core.model.PaisModel;
-
-public interface PaisRepository extends JpaRepository<PaisModel, Integer>{
-
-}
